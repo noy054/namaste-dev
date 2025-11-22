b22b732c01f25ad3d416faf7ec83ae5350825a39
noy054 <jleprog39@gmail.com>
2025-11-22 16:54:40 +0200

    Episode 02

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 const heading = React.createElement(
   "h1",
   {
     id: "title",
+    key: 1,
   },
   "Heading 1"
 );
@@ -9,6 +13,7 @@ const heading2 = React.createElement(
   "h2",
   {
     id: "title",
+    key: 2,
   },
   "Heading 2"
 );
@@ -16,11 +21,12 @@ const container = React.createElement(
   "div",
   {
     id: "container",
+    key: 3,
   },
   [heading, heading2]
 );
-console.log(heading);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const root = createRoot(document.getElementById("root"));
 //passing a react element inside the root
 //async defer
 root.render(container);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 #Namaste React
+
+#Parcle
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code splitting
+- Differential Bundling - support older browsers
+- Dignostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
